7f77a803271545670288c4c4cff564135a161254
AhmadoshHamad <1212621@student.birzeit.edu>
2025-02-28 18:07:12 +0200

    small clean code updates

--- flask-websocket/app.py
+++ flask-websocket/app.py
@@ -7,7 +7,7 @@ from datetime import datetime # Add this line to import datetime module
 # from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity # type: ignore
 
 from flask_cors  import CORS, cross_origin
-
+from models import * 
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -16,57 +16,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app,supports_credentials=True)
 
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    email = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
-
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    document_content = db.Column(db.Text, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
-class Sheet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-class Cell(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    row = db.Column(db.Integer, nullable=False)
-    column = db.Column(db.Integer, nullable=False)
-    value = db.Column(db.String(30), nullable=False)
-    sheet_id = db.Column(db.Integer, db.ForeignKey('sheet.id'), nullable=False)
-    sheet = db.relationship('Sheet', backref=db.backref('cells', lazy=True))
-
-class UserSheet(db.Model):
-    __tablename__ = 'user_sheet'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    sheet_id = db.Column(db.Integer, db.ForeignKey('sheet.id'), primary_key=True)
-    # Define relationships
-    user = db.relationship('User', backref=db.backref('user_sheets', lazy=True))
-    sheet = db.relationship('Sheet', backref=db.backref('user_sheets', lazy=True))
-
-class SheetInvitation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    sheet_id = db.Column(db.Integer, db.ForeignKey('sheet.id'), nullable=False)
-    status = db.Column(db.Integer,default=0)
-    user = db.relationship('User', backref=db.backref('invitations', lazy=True))
-    sheet = db.relationship('Sheet', backref=db.backref('invitations', lazy=True))
-
-class UserDoc(db.Model):
-    __tablename__ = 'user_doc'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    doc_id = db.Column(db.Integer, db.ForeignKey('document.id'), primary_key=True)
-    # Define relationships
-    user = db.relationship('User', backref=db.backref('user_docs', lazy=True))
-    document = db.relationship('Document', backref=db.backref('user_docs', lazy=True))
 
 with app.app_context():
     # // delete db
--- test2/vite-project/src/pages/DocsEditorPage.jsx
+++ test2/vite-project/src/pages/DocsEditorPage.jsx
@@ -91,26 +91,7 @@ const DocsEditorPage = () => {
         <>
             <SideBar isDocs={true} />
             <div className="extended" >
-                {/* <textarea className='w-full' name="" id="" onChange={handleChange}  value={data?.document_content} ></textarea> */}
-                <Editor
-                    apiKey='wsydvry22j6a0sfnfvdqruw5e722cy7bsuqomzhhqqhl0k9p'
-                    init={{
-                        height: '1000px',
-                        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                        tinycomments_mode: 'embedded',
-                        tinycomments_author: 'Author name',
-                        mergetags_list: [
-                            { value: 'First.Name', title: 'First Name' },
-                            { value: 'Email', title: 'Email' },
-                        ],
-                        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-                    }}
-                    initialValue={data?.document_content}
-                    // initialValue={message}
-                    onEditorChange={handleChange}
-                    value={message} 
-                />
+                <textarea className='w-full ml-10' name="" id="" onChange={(e) => handleChange(e.target.value)} value={message}></textarea>
             </div>
         </>
     );
